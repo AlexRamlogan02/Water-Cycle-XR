12671b01e61bdd0b1d7fb9658ec94f0f802cf52d
Gabrielle Pariag <116374815+GabriellePariag@users.noreply.github.com>
2023-11-10 17:07:26 -0500

    Add files via upload
    final draft of landing page

--- index.js
+++
@@ -1,7 +0,0 @@
-const startButton = document.getElementById("startButton");
-
-startButton.addEventListener("click", function () {
-    redirect(
-        window.location.href = `scene.html`
-    );
-  });
